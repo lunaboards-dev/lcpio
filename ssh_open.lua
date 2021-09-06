9291997a9f72bca3929a094c0cb0708521d6bd5f
Sam Roxanne <lunar_sam@protonmail.com>
2021-09-06 04:29:40 -0500

    remove debug print

--- ssh_open.lua
+++ ssh_open.lua
@@ -4,7 +4,6 @@ local function ssh_open(addr, path, mode)
 		return f
 	elseif mode == "w" then
 		local f = io.popen(string.format("%q %q %q", sshcommand, addr, string.format("cat - > %q", path)), "w")
-		lcpio.warning(string.format("%q", string.format("%q %q %q", sshcommand, addr, string.format("cat - > %q", path))))
 		return f
 	end
 	error("invalid mode")
